77447aab27fe995b81aa7a27617a832fd4c12228
cperales <cperales@student.42madrid.com>
2025-01-13 18:33:41 +0100

    arreglada la norma

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cperales <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: cperales <cperales@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 16:48:38 by cperales          #+#    #+#             */
-/*   Updated: 2024/10/15 16:50:53 by cperales         ###   ########.fr       */
+/*   Updated: 2025/01/13 18:31:44 by cperales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
@@ -32,12 +31,3 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 			*(dst++) = *(sr++);
 	return (dest);
 }
-
-/*
-int main()
-{
-	char s[12] = "mundo";
-	char d[12] = "hola";
-	ft_memmove(d, s, 3);
-	printf("%s\n", d);
-}*/
